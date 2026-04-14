1ebe5360db6210e74617989ea63721759a5427cd
JoaoMarceloAngeli <joaomarcelo071012@gmail.com>
2026-04-14 14:05:41 -0300

    Pirâmide
    Projeto simples em Java que ao incluir uma altura e uma base, o programa calcula o volume.

---
+++ ProjetoPiramide/src/entidade/Piramide.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entidade;
+
+/**
+ *
+ * @author Pedro Francisco
+ */
+public class Piramide {
+     public float Volume(float Base, float Altura){
+        return((Base*Altura)/3);
+    }
+}
