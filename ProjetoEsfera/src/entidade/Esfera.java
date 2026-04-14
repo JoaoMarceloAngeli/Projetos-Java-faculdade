97947ff949867e47fd2011cc87058283744a8be7
JoaoMarceloAngeli <joaomarcelo071012@gmail.com>
2026-04-14 14:26:49 -0300

    Esfera
    Projeto simples em Java que com a inserção de um raio, calcula o raio e o volume.

---
+++ ProjetoEsfera/src/entidade/Esfera.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entidade;
+import visao.TelaDoProjetoEsfera;
+/**
+ *
+ * @author Pedro Francisco
+ */
+public class Esfera {
+    public float Raio (float Raio){
+    return (float) (4* 3.1415 * (Raio * Raio));
+    }
+}
---
+++ ProjetoEsfera/src/entidade/Esfera02.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entidade;
+import visao.TelaDoProjetoEsfera;
+/**
+ *
+ * @author Pedro Francisco
+ */
+public class Esfera02 {
+    public float Volume(float Raio){
+        return ((4/3)*3*Raio* Raio* Raio);
+    }
+    
+}
