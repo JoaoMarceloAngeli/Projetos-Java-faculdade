c630b0b611b4bb9e0392478dfb7f871095755f6b
JoaoMarceloAngeli <joaomarcelo071012@gmail.com>
2026-04-15 13:51:53 -0300

    Projeto retangulo
    Projeto simples em Java onde ao se inserir a largura e comprimento, calcula o perímetro e a área

---
+++ ProjetoRetangulo/src/entidade/Retangulo02.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entidade;
+
+/**
+ *
+ * @author Pedro Francisco
+ */
+public class Retangulo02 {
+     public float calcular(float Comprimento, float Largura){
+        return(Largura *Comprimento);
+    }
+}
